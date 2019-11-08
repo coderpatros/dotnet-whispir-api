862d5b2780b32c730e07f74b71ffb005234ddbf9
Patrick Dwyer <pat@patdwyer.com.au>
2019-11-09 07:21:38 +1000

    Refactor classes to individual cs files

---
+++ Patros.WhispirApi.HttpClient/WhispirApiHttpClientOptions.cs
@@ -0,0 +1,9 @@
+namespace Patros.WhispirApi
+{
+    public class WhispirApiHttpClientOptions
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
